e75b358094e00751e1fbd089a6212519e3fdd0c1
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-23 22:16:56 +0530

    correct the format

--- HANDSONS/SEQUENCE/Q1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const readlineSync = require("readline-sync");
-let n = readlineSync.question("Enter the name: ");
-let age = readlineSync.question("Enter the age: ");
-let fav = readlineSync.question("Enter the favourite: ");
-console.log("\nName:", n);
-console.log("Age:", age);
-console.log("Favourite:", fav);
-//# sourceMappingURL=Q1.js.map
--- HANDSONS/SEQUENCE/Q1.ts
+++
@@ -1,9 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let n = readlineSync.question("Enter the name: ");
-let age = readlineSync.question("Enter the age: ");
-let fav = readlineSync.question("Enter the favourite: ");
-
-console.log("\nName:", n);
-console.log("Age:", age);
-console.log("Favourite:", fav);
